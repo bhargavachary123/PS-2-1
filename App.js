0681983c58bf319f90f1ff78adf6438b4840b776
pavansingireddy <singireddypavankumarreddy500@gmail.com>
2023-11-06 15:53:13 +0530

    pavan code

--- App.js
+++ App.js
@@ -1,46 +1,60 @@
-import Animated, {
-  useSharedValue,
-  withTiming,
-  useAnimatedStyle,
-  Easing,
-} from "react-native-reanimated";
-import { View, Button } from "react-native";
+import React from 'react';
+import { View, Text, Button } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import myTabs from './tabRoots';
 
-export default function AnimatedStyleUpdateExample(props) {
-  const randomWidth = useSharedValue(10);
 
-  const config = {
-    duration: 500,
-    easing: Easing.bezier(0.5, 0.01, 0, 1),
-  };
-
-  const style = useAnimatedStyle(() => {
-    return {
-      width: withTiming(randomWidth.value, config),
-    };
-  });
-
+function SettingsScreen({ navigation }) {
   return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: "center",
-        justifyContent: "center",
-        flexDirection: "column",
-      }}
-    >
-      <Animated.View
-        style={[
-          { width: 100, height: 80, backgroundColor: "black", margin: 30 },
-          style,
-        ]}
-      />
-      <Button
-        title="toggle"
-        onPress={() => {
-          randomWidth.value = Math.random() * 350;
-        }}
-      />
+    <View>
+      <Text>Settings Screen</Text>
+      <Button title="Open Drawer" onPress={() => navigation.openDrawer()} />
     </View>
   );
 }
+function rightsScreen({ navigation }) {
+  return (
+    <View>
+      <Text>rightsScreen</Text>
+    </View>
+  );
+}
+function legalaidScreen({ navigation }) {
+  return (
+    <View>
+      <Text>leagalaidScreen</Text>
+    </View>
+  );
+}
+function rehabiltationScreen({ navigation }) {
+  return (
+    <View>
+      <Text>rehabiltationScreen</Text>
+    </View>
+  );
+}
+
+const Drawer = createDrawerNavigator();
+const Tab = createMaterialBottomTabNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="myTabs">
+        {/* Add your drawer screens here */}
+        <Drawer.Screen name="myTabs" component={myTabs} />
+        <Drawer.Screen name="Settings" component={SettingsScreen} />
+        <Drawer.Screen name="rights" component={rightsScreen} />
+        <Drawer.Screen name="legalaid" component={legalaidScreen} />
+        <Drawer.Screen name="rehabiltation" component={rehabiltationScreen} />
+        {/* Add more screens as needed */}
+      </Drawer.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
