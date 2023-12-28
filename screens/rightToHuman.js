aeec03c10af9f3d09327fae9cf0eda25a5c5ecd0
sivani-1 <147125105+sivani-1@users.noreply.github.com>
2023-12-28 15:15:29 +0530

    hm

--- screens/rightToHuman.js
+++ screens/rightToHuman.js
@@ -10,7 +10,7 @@ export default function rightToHuman() {
               and recommended by the supreme court of india which has categorical declared that prisioners shall not be treated as non-persons.
               right to integrity of body; immunity from use of repression and personal abuse, whether by custodial staff or by prisioners.
               </Text>
-              <Image source={{uri: 'https://psacunion.ca/sites/psac/files/brochure_cover.png',
+              <Image source={{uri: 'https://image.slidesharecdn.com/11-190902065309/85/human-rights-dignity-and-the-common-good-5-320.jpg?cb=1665790375',
                           width: 500,
                           height: 700,}}/>
             </View>
@@ -18,10 +18,11 @@ export default function rightToHuman() {
                       width={700}
                       height={300}
                       play={false}
-                      videoId={'kGShnVjzq94'}
+                      videoId={'qQXdn7WYFSM'}
                   />
         </View>
     )
+
 }
 
 const styles = StyleSheet.create({
